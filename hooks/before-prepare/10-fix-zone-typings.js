fcf10d22e4e736ec3e5ebf2e4511952a8b54a638
Hristo Deshev <hristo@deshev.com>
2015-12-07 15:31:46 +0200

    Check if livesyncing in zone.d.ts hook to avoid endless recompiles.

--- hooks/before-prepare/10-fix-zone-typings.js
+++ hooks/before-prepare/10-fix-zone-typings.js
@@ -1,7 +1,9 @@
 var path = require('path');
 var shelljs = require('shelljs');
 
-module.exports = function() {
-    var zoneDts = path.join('node_modules', 'angular2', 'typings', 'zone', 'zone.d.ts');
-    shelljs.sed('-i', /.*reference.*path.*es6-shim.*\n/g, '', zoneDts);
+module.exports = function ($logger, $projectData, $usbLiveSyncService) {
+	if (!$usbLiveSyncService.isInitialized) {
+        var zoneDts = path.join('node_modules', 'angular2', 'typings', 'zone', 'zone.d.ts');
+        shelljs.sed('-i', /.*reference.*path.*es6-shim.*\n/g, '', zoneDts);
+    }
 }
