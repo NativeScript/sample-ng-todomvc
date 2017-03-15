fdd667f984f6140b3e4bf57b2baf1b9367226dad
Stanimira Vlaeva <stanimira.vlaeva@gmail.com>
2017-03-15 10:34:34 +0200

    fix: import NativeScriptModule from nativescript-angular/nativescript.module (#17)

--- app/app.ts
+++ app/app.ts
@@ -1,5 +1,6 @@
 // this import should be first in order to load some required settings (like globals and reflect-metadata)
-import { platformNativeScriptDynamic, NativeScriptModule } from "nativescript-angular/platform";
+import { platformNativeScriptDynamic } from "nativescript-angular/platform";
+import { NativeScriptModule } from "nativescript-angular/nativescript.module";
 import { NgModule } from "@angular/core";
 import { MainPage } from "./main-page";
 import { Checkbox } from './checkbox';
